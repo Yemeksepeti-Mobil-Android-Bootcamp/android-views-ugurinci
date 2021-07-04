598381d054cd2262aaf5620e519921f4892f8940
ugurinci <ugur.inci.96@gmail.com>
2021-07-04 18:34:54 +0300

    first commit

---
+++ app/src/main/java/com/ugurinci/odev1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ugurinci.odev1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_screen1)
+//        setContentView(R.layout.layout_screen2)
+//        setContentView(R.layout.layout_screen3)
+    }
+}
