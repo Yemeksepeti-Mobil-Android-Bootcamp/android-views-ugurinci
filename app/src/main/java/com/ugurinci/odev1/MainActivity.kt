ca1ca75988ddfcef41485d7b4de8999171805117
ugurinci <ugur.inci.96@gmail.com>
2021-07-05 23:11:55 +0300

    resource rename commit

--- app/src/main/java/com/ugurinci/odev1/MainActivity.kt
+++ app/src/main/java/com/ugurinci/odev1/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_screen1)
-//        setContentView(R.layout.layout_screen2)
-//        setContentView(R.layout.layout_screen3)
+        setContentView(R.layout.layout_start)
     }
 }
